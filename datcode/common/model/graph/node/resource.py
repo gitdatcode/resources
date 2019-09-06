ea433eef19497d5182741978dcb15db2c0c915ea
emehrkay <mark.henderson@8trk.com>
2019-09-05 22:32:48 -0400

    adding order to results

--- datcode/common/model/graph/node/resource.py
+++ datcode/common/model/graph/node/resource.py
@@ -138,6 +138,7 @@ class ResourceMapper(BaseNodeMapper, HasOwnership):
             total_results = 0
 
         query.RETURN('DISTINCT(resource)', 'user')
+        query.ORDERBY('resource.date_created', 'DESC')
         query.SKIP(skip).LIMIT(limit)
         results = self.mapper.query(pypher=query)
         result_data = []
